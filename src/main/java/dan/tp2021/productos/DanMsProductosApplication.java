6f741a712674d9b59ee44a84ded478a22e8ffb88
mart-dominguez <mdomingu@gmail.com>
2021-03-22 22:51:09 -0300

    microservicio gestion productos

---
+++ src/main/java/dan/tp2021/productos/DanMsProductosApplication.java
@@ -0,0 +1,13 @@
+package dan.tp2021.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanMsProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dan/tp2021/productos/DanMsProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.tp2021.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
